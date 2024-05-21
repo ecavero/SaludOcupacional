3acd5c71dbaa679e0fd68dc726d88b39a6fb00b0
Eduardo Cavero <ecavero@gmail.com>
2024-05-20 22:13:19 -0500

    Crear vista pacientes

---
+++ BaseDeDatos/Query_Crear_View.sql
@@ -0,0 +1,11 @@
+CREATE VIEW v_Paciente
+AS
+
+SELECT pe.idPersona, pe.dni, pe.apellidoPaterno, pe.apellidoMaterno, u.departamento, u.provincia, u.distrito
+FROM Persona pe
+INNER JOIN Paciente pa
+ON pa.idPersona = pe.idPersona
+INNER JOIN Ubigeo u
+ON u.idUbigeo = pe.idUbigeo
+GO
+         
