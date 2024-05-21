6803ba5b11bdc4a2c83f0ce651ea95d8e041e537
Eduardo Cavero <ecavero@gmail.com>
2024-05-20 19:48:59 -0500

    Agregando proyecto de N Capas

---
+++ SaludOcupacional/SaludOcupacional_Model/Conexion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaludOcupacional_Model
+{
+    public class Conexion
+    {
+        public string ObtenerCadenaConexion()
+        {
+            string cadenaConexion = ConfigurationManager.AppSettings["db"];
+            cadenaConexion = System.Environment.ExpandEnvironmentVariables(cadenaConexion);
+            return cadenaConexion;
+        }
+    }
+}
---
+++ SaludOcupacional/SaludOcupacional_View/Form1.cs
@@ -0,0 +1,10 @@
+namespace SaludOcupacional_View
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
